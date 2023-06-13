38584dd3cbd74f4bcc4ea84b9972235c21dff09d
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-06-13 22:50:39 +0600

    add nav items and footer

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,16 +1,16 @@
-import {
-  RiFacebookBoxFill,
-  RiGithubFill,
-  RiLinkedinBoxFill,
-} from "react-icons/ri";
+import { RiFacebookFill, RiGithubFill, RiLinkedinFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 left-0 right-0 h-12 border-t border-p4 px-6 flex justify-between items-center text-s1 text-lg">
-      <div className="border-r border-p4 h-full flex justify-start items-center">
+      <div className="border-r border-p4 h-full flex justify-start items-center transition-all">
         find me in:
-        <RiLinkedinBoxFill />
-        <RiFacebookBoxFill />
+        <Link className="ml-5 w-14 hover:text-white border-x h-full border-p4 flex justify-center items-center">
+          <RiLinkedinFill size={24} />
+        </Link>
+        <Link className="w-14  h-full hover:text-white border-p4 flex justify-center items-center">
+          <RiFacebookFill size={24} />
+        </Link>
       </div>
       <div
         className="w-2/12 border-l h-full flex justify-end items-center border-p4
