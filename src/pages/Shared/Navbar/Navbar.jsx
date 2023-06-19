432a3ed9842fc60bf69eba38d19c81fe1d59f71e
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-06-19 17:09:32 +0600

    navbar responsive complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import { RiMenuFill } from "react-icons/ri";
+import { useState } from "react";
+import { RiCloseFill, RiMenuFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import ActiveNav from "../../../components/ActiveLink/ActiveNav";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navItems = (
     <>
       <li>
@@ -18,11 +21,39 @@ const Navbar = () => {
   return (
     <div className="text-s1  text-lg sticky top-0 h-14 bg-p2 z-50 flex justify-between items-center border-y border-p4">
       {/* responsive menu  */}
-      <nav className="lg:w-[21%] w-full lg:pl-6 lg:px-0 px-10 flex justify-between items-center">
+      <div className="lg:w-[21%] w-full lg:pl-6 lg:px-0 px-10 flex justify-between items-center">
         <span>_mahamudul-hasan</span>
-        <RiMenuFill size={30} />
-      </nav>
+        <span className="lg:hidden">
+          {isMenuOpen ? (
+            <RiCloseFill onClick={() => setIsMenuOpen(!isMenuOpen)} size={36} />
+          ) : (
+            <RiMenuFill onClick={() => setIsMenuOpen(!isMenuOpen)} size={30} />
+          )}
+        </span>
+      </div>
 
+      {/* responsive menu  */}
+      {isMenuOpen && (
+        <nav
+          data-aos="fade-left"
+          className="top-14 pt-4 right-0 bottom-0 w-full h-[calc(100vh-7.1rem)]  absolute bg-p2"
+        >
+          <ul className="responsive-menu">
+            <Link to={"/"} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <li>_home</li>
+            </Link>
+            <Link to={"/"} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <li>_about-me</li>
+            </Link>
+            <Link to={"/projects"} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <li>_projects</li>
+            </Link>
+            <Link to={"/contact"} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <li>_contact</li>
+            </Link>
+          </ul>
+        </nav>
+      )}
       {/* large device menu  */}
       <nav className="w-[calc(100vw-21%)] mx-auto border-x border-p4 h-full hidden lg:flex justify-between items-center">
         <ul className="menu_nav-items">{navItems}</ul>
